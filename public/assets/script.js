b76cf383e3cb296ac773f2075682177fbe744c5a
cy-r0x <payel15-5106@diu.edu.bd>
2025-06-24 15:21:26 +0600

    applied desgin 24/06/2025

--- public/assets/script.js
+++
@@ -1,7 +0,0 @@
-const li = document.querySelectorAll('li');
-li.forEach((element) => {
-    element.addEventListener('click',()=>{
-        console.log(element.innerText);
-    })
-});
- 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-  <title>Document</title>
-</head>
-<body>
-  <div class="bg-zinc-900 text-white flex justify-center py-4">
-    <ul class="flex gap-4" id='nav-ul'>
-      <li>Home</li>
-      <li>Projects</li>
-      <li>About Me</li>
-      <li>Contact</li>
-    </ul>
-  </div>
-  <script src="{{asset('assets/script.js')}}"></script>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="{{ asset('assets/style.css') }}" />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr"
+            crossorigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Jura:wght@300..700&display=swap"
+            rel="stylesheet"
+        />
+
+        <title>Prantor's Portfolio</title>
+    </head>
+    <body>
+        <div class="container-fluid min-vh-100 p-4">
+            <div class="d-flex gap-3 gap-md-4 justify-content-end">
+                <button class="icons">
+                    <a href="https://github.com/cy-r0x" target="_blank">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="28"
+                            height="28"
+                            fill="#2b2b2b"
+                            class="bi bi-github"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
+                            />
+                        </svg>
+                    </a>
+                </button>
+                <button class="icons">
+                    <a href="httsp://facebook.com/cyr0xx" target="_blank">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="28"
+                            height="28"
+                            fill="#2b2b2b"
+                            class="bi bi-facebook"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"
+                            />
+                        </svg>
+                    </a>
+                </button>
+                <button class="icons">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="28"
+                        height="28"
+                        fill="#2b2b2b"
+                        class="bi bi-moon-fill"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <div
+                class="main-content d-flex justify-content-between align-items-center"
+            >
+                <div class="text-center text-md-start">
+                    <img
+                        src="{{ asset('assets/avatar.jpg') }}"
+                        alt="Profile Avatar"
+                        class="avatar cover"
+                    />
+                    <p class="jura mt-3 ms-md-2">fig: cyr0x</p>
+                </div>
+                <div class="text-center text-md-end parent-container">
+                    <p class="jura text-custom glass">Info</p>
+                    <p class="jura text-custom">Projects</p>
+                    <p class="jura text-custom">Resume</p>
+                </div>
+            </div>
+        </div>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
